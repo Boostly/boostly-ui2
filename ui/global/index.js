cc0f5d07a7160c21ef870dca92054a5268b093af
Mikey Murphy <mikeyamadeo@gmail.com>
2019-03-09 21:30:55 -0700

    conditionally load fonts if in browser

--- ui/global/index.js
+++ ui/global/index.js
@@ -1,10 +1,12 @@
-import { injectGlobal } from 'emotion'
-import WebFont from 'webfontloader'
-import { baseFontSize } from '../settings'
+import { injectGlobal } from "emotion";
+import WebFont from "webfontloader";
+import { baseFontSize } from "../settings";
 
-WebFont.load({
-  google: { families: ['Work+Sans:400,700', 'Righteous', 'sans-serif'] }
-})
+if (window) {
+  WebFont.load({
+    google: { families: ["Work+Sans:400,700", "Righteous", "sans-serif"] }
+  });
+}
 
 injectGlobal`
   * {
@@ -12,4 +14,4 @@ injectGlobal`
   }
   html { font-size: ${baseFontSize}px }
   body { margin: 0; }
-`
+`;
