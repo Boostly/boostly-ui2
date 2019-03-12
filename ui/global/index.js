2f3abe29dac34e7b5e11633ab4ff4aebde2853de
Mikey Murphy <mikeyamadeo@gmail.com>
2019-03-12 15:40:25 -0600

    remove global injection

--- ui/global/index.js
+++ ui/global/index.js
@@ -1,14 +1,4 @@
 import React, { useEffect } from 'react'
-import { injectGlobal } from '@emotion/core'
-import { baseFontSize } from '../settings'
-
-injectGlobal`
-  * {
-    box-sizing: border-box;
-  }
-  html { font-size: ${baseFontSize}px }
-  body { margin: 0; }
-`
 
 export const LoadFont = () => {
   useEffect(() => {
